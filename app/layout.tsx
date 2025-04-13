785998de971c319b9ce9a37c83f2286841d1851a
nikcto <nikcto951@gmail.com>
2025-04-13 15:53:31 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,15 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://iceberg-photo-reveal.vercel.app'),
   title: "Главные тайны Тураходжаева данила и Мифтаховой Ксении",
   description: "Чем я занимаюсь?",
   openGraph: {
     title: "Главные тайны Тураходжаева данила и Мифтаховой Ксении",
     description: "Чем я занимаюсь?",
+    url: 'https://iceberg-photo-reveal.vercel.app',
+    siteName: 'Iceberg Photo Reveal',
+    locale: 'ru_RU',
     type: "website",
     images: [
       {
@@ -20,6 +24,12 @@ export const metadata: Metadata = {
         alt: "Главные тайны Тураходжаева данила и Мифтаховой Ксении"
       }
     ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Главные тайны Тураходжаева данила и Мифтаховой Ксении",
+    description: "Чем я занимаюсь?",
+    images: ["/og-image.jpg"],
   }
 };
 
